837a354e5afd48276070a7c070206ecb7674076e
Michael Arandityo N <michaelarandityonugroho@gmail.com>
2019-08-04 08:43:46 +0700

    delete todos

--- src/main/java/api/usermodule/services/UserService.java
+++ src/main/java/api/usermodule/services/UserService.java
@@ -32,53 +32,12 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private UserModuleRepository userModuleRepository;
-
-    public User findOne(Long userId) { return userRepository.findOne(userId); }
-
-    // TODO : optimize post and put service
-
-    private void generateUserModules(User user, UserDTO userDTO) {
-        List<UserModule> userModules = new ArrayList<>();
-        if (user.getModules().size() > 0) {
-            user.getModules().clear();
-            userRepository.save(user);
-        }
-        Integer moduleOrder = userDTO.getUserId() != null ? userModuleRepository.findFirstByUserModulePK_User_UserIdOrderByUserModulePK_ModuleOrderDesc(userDTO.getUserId()).getUserModulePK().getModuleOrder() : 1;
-            for (UserModuleDTO userModuleDTO : userDTO.getModules()) {
-                UserModule userModule = new UserModule();
-                UserModulePK userModulePK = new UserModulePK();
-                Module module = new Module();
-                User userTemp = new User();
-                userTemp.setUserId(userDTO.getUserId());
-                module.setModuleId(userModuleDTO.getModuleDTO().getModuleId());
-                userModule.setModule(module);
-                userModulePK.setUser(userTemp);
-                userModulePK.setModuleOrder(userModuleDTO.getUserModulePKDTO() != null && userModuleDTO.getUserModulePKDTO().getModuleOrder() != null ? userModuleDTO.getUserModulePKDTO().getModuleOrder() : moduleOrder != null ? moduleOrder + 1 : 1);
-                userModule.setUserModulePK(userModulePK);
-                if(user.getUserId() != null && userModuleRepository.findAllByUserModulePK(userModulePK).size() < 1) {
-                    if (user.getUserId() == null) {
-                        throw new NullPointerException("User module already exist");
-                    }
-                    userModules.add(userModule);
-                    moduleOrder++;
-                } else {
-                    userModules.add(userModule);
-                    moduleOrder++;
-
-                }
-            }
-        user.setModules(new HashSet<>(userModules));
-    }
-
     private void convertFromDTO(User user, UserSaveDTO userDTO){
         user.setUserCode(userDTO.getUserCode());
         user.setUserName(userDTO.getUserName());
-//        if (userDTO.getModules() != null && userDTO.getModules().size() > 0) {
-//            generateUserModules(user, userDTO);
-//        }
     }
+
+    public User findOne(Long userId) { return userRepository.findOne(userId); }
 
     public User insertUser(UserSaveDTO userDTO) {
         User user = new User();
